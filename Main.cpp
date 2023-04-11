e1c83f914c95602f7b76f54acb62329501de5c26
NguyenMinhNhaHCMUS <117451019+NguyenMinhNhaHCMUS@users.noreply.github.com>
2023-04-12 04:38:24 +0700

    Gọn hơn xíu

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <stdlib.h>
 #include <conio.h>
+#include <string>
 using namespace std;
 
 #define BOARD_SIZE 12 //Kích thước ma trận bàn cờ
 #define LEFT 3 //Tọa độ trái màn hình bàn cờ
 #define TOP 1 //Tọa độ trên màn hình bàn cờ
+#define OPTION_HIGH 2
+#define OPTION_WIDTH  20
 
 //Khai báo kiểu dữ liệu
 struct _POINT { int x, y, c; }; //x: tọa độ dòng, y: tọa độ cột, c: đánh dấu
@@ -13,14 +16,33 @@ _POINT _A[BOARD_SIZE][BOARD_SIZE]; //Ma trận bàn cờ
 bool _TURN; //true là lượt người thứ nhất và false là lượt người thứ hai
 int _COMMAND; //Biến nhận giá trị phím người dùng nhập
 int _X, _Y; //Tọa độ hiện hành trên màn hình bàn cờ
+struct MENU
+{
+	string opt1;
+	string opt2;
+	string opt3;
+	string opt4;
+};
+MENU menu = { "NEW GAME","LOAD GAME","ABOUT","EXIT" };
+int _OPTION; //Thứ tự hộp menu
+int Score1;
+int Score2;
+string Player1_name;
+string Player2_name;
 
 //Hàm View
 void FixConsoleWindow();
 int ProcessFinish(int pWhoWin);
-int AskContinue();
+void AskContinue();
 void GotoXY(int x, int y);
 void SetColor(int backgound_color, int text_color);
 void HighLight(int x, int y, int w, int h, int color);
+void DrawOption(int x, int y, int w, int h, int b_color, int t_color, string s);
+void DrawMenu(int x, int y, int w, int h, MENU m);
+void Draw_newgame_opt(int x, int y, int w, int h);
+void ChangeBackgrColor();
+void Hightlight_Play_turn(int x, int y, int w, int h, int color, int player);
+//void DrawRule(int x, int y, int w, int h);
 //Hàm Control
 void StartGame();
 void ExitGame();
@@ -28,21 +50,27 @@ void MoveRight();
 void MoveLeft();
 void MoveUp();
 void MoveDown();
+void MenuUp();
+void MenuDown();
 //Hàm Model
 int CheckTick(int pX, int pY);
 int CheckBoard(int pX, int pY);
 int TestBoard();
+void ResetData();
+void Menu();
 
 void main()
 {
 	FixConsoleWindow();
-	StartGame();
+	ChangeBackgrColor();
+	Menu();
 	bool validEnter = true;
 	while (1)
 	{
 		_COMMAND = toupper(_getch());
 		if (_COMMAND == 27)
 		{
+			SetColor(15, 0);
 			ExitGame();
 			return;
 		}
@@ -202,7 +230,7 @@ void main()
 			}
 			else if (_COMMAND == 13) //Nhấn Enter
 			{
-				switch(CheckBoard(_X, _Y))
+				switch (CheckBoard(_X, _Y))
 				{
 				case -1:
 					HighLight(_X - 1, _Y, 3, 1, 15);
@@ -224,16 +252,40 @@ void main()
 				{
 					switch (ProcessFinish(TestBoard()))
 					{
+					case 2:
+					{
+						if (_TURN == true)
+						{
+							Hightlight_Play_turn(55, _A[0][BOARD_SIZE - 1].y, 60, 12, 15, 2);
+							Hightlight_Play_turn(55, _A[0][BOARD_SIZE - 1].y, 60, 12, 14, 1);
+						}
+						else
+						{
+							Hightlight_Play_turn(55, _A[0][BOARD_SIZE - 1].y, 60, 12, 15, 1);
+							Hightlight_Play_turn(55, _A[0][BOARD_SIZE - 1].y, 60, 12, 14, 2);
+						}
+						break;
+					}
 					case -1:
 					case 1:
 					case 0:
-						if (AskContinue() != 'Y')
+						AskContinue();
+						while(1)
 						{
-							ExitGame();
-							return;
+							int temp = toupper(_getch());
+							if (temp == 'N')
+							{
+								SetColor(15, 0);
+								ExitGame();
+								return;
+							}
+							else if (temp == 'Y')
+							{
+								SetColor(15, 0);
+								StartGame();
+								break;
+							}
 						}
-						else
-							StartGame();
 					}
 				}
 				validEnter = true; //Mở khóa
